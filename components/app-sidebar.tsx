43ca5a66f8747210a2806a0e03f12e8cfb645459
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-07-25 17:51:34 -0400

    improve padding and margin
    adds padding/margin to header button, sidebar trigger button, and
    sidebar content which wraps thread-list

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -22,13 +22,13 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <div className="flex items-center justify-between">
           <SidebarMenu>
-            <SidebarMenuItem>
+            <SidebarMenuItem className="mr-2">
               <SidebarMenuButton size="lg" asChild>
                 <Link href="https://assistant-ui.com" target="_blank">
                   <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                     <MessagesSquare className="size-4" />
                   </div>
-                  <div className="flex flex-col gap-0.5 leading-none">
+                  <div className="flex flex-col gap-0.5 leading-none mr-6">
                     <span className="font-semibold">assistant-ui</span>
                   </div>
                 </Link>
@@ -46,7 +46,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </SidebarTrigger>
         </div>
       </SidebarHeader>
-      <SidebarContent>
+      <SidebarContent className="px-2">
         <ThreadList />
       </SidebarContent>
       <SidebarFooter>
